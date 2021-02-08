86deb7cf4f09c7a591be64399026551a4441b21b
garbimelida <78142837+garbimelida@users.noreply.github.com>
2021-02-08 01:55:05 +0100

    Menu portfolio - Js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+const portfolio = document.querySelector( '.portfolio')
+const submenu2 = document.querySelector( '.submenu2')
+
+portfolio.addEventListener('click',function(){
+    submenu2.classList.toggle('ver')
+})
