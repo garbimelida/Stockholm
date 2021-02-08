18717fd1357973cde3a7e16674703e772f61ae58
garbimelida <78142837+garbimelida@users.noreply.github.com>
2021-02-08 12:15:41 +0100

    SCSS y TS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
+"use strict"
 
 
-const portfolio = document.querySelector( '.portfolio')
-const submenu2 = document.querySelector( '.submenu2')
-
-portfolio.addEventListener('click',function(){
-    submenu2.classList.toggle('ver')
-})
+var portfolio = document.querySelector('.portfolio');
+var submenu2 = document.querySelector('.submenu2');
+portfolio.addEventListener('click', function () {
+    submenu2.classList.toggle('ver');
+});
